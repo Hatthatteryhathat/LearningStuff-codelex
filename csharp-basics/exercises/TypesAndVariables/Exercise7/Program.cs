aac36db65cdf06c3d97640d01dcb3a28fda9bd51
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-07-27 21:02:38 +0300

    Compleated Exercise7

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string:: ");
+            string userInputString = Console.ReadLine();
+
+            int amountOfUpperLetters = 0;
+            foreach (char chr in userInputString)
+            {
+                if (Char.IsUpper(chr)) 
+                {
+                    amountOfUpperLetters++;
+                }
+            }
+
+            Console.WriteLine($"---\n Amount of uppercase letters is ::: {amountOfUpperLetters}");
+            Console.ReadKey();
+        }
+    }
+}
