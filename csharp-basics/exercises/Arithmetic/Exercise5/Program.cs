26013525efd956396cefe7cd53cfc87800ba015b
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-01 15:00:22 +0300

    Completed Exercise #5

---
+++ csharp-basics/exercises/Arithmetic/Exercise5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumberGen = new Random();
+            int random1To100 = randomNumberGen.Next(1, 100);
+
+            Console.WriteLine("I'm thinking of a number between 1-100.  Try to guess it.");
+            string usersGuess = Console.ReadLine();
+
+            if (random1To100 == Int32.Parse(usersGuess))
+            {
+                Console.WriteLine("You guessed it!  What are the odds?!?");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, you are too {(random1To100 < Int32.Parse(usersGuess) ? "high" : "low")}." +
+                    $"  I was thinking of {random1To100}.");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
