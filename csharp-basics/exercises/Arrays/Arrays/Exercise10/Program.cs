05c2bc4c04c25281f2de5d483793fe42de1a5478
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-08 17:58:32 +0300

    Completed Exercise #10

---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 92, 6, 73, -77, 81, -90, 99, 8, -85, 34 };
+            int countPositive = 0;
+            int sumNegative = 0;
+
+            for (int i = 0; i < array.Length; i++) 
+            {
+                if (array[i] > 0)
+                {
+                    countPositive++;
+                }
+                else 
+                {
+                    sumNegative += array[i];
+                }
+            }
+
+            Console.WriteLine($"Positive numbers : {countPositive}");
+            Console.WriteLine($"Sum of negative numbers : {sumNegative}");
+            Console.ReadKey();
+        }
+    }
+}
