18319f9d04e41c2ef33afb41b896060dc5380175
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-08 17:54:10 +0300

    Completed Exercise #9

---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = {"samuel", "MABELLE", "letitia", "mErIDith"};
+            Console.WriteLine("Before : " + string.Join(", ", names));
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                string name = names[i].ToLower();
+                name = name.ToLower();
+                names[i] = char.ToUpper(name[0]) + name.Substring(1,name.Length - 1);
+            }
+
+            Console.WriteLine("After : " + string.Join(", ", names));
+            Console.ReadKey();
+        }
+    }
+}
