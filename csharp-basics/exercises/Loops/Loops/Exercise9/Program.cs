82c623e0a876b0b82d468c8cb4e4c5b6147537b2
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-11 17:46:32 +0300

    Completed Exercise #9

---
+++ csharp-basics/exercises/Loops/Loops/Exercise9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RollTwoDice diceGame = new RollTwoDice();
+
+            diceGame.DiceGame();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ csharp-basics/exercises/Loops/Loops/Exercise9/RollTwoDice.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    internal class RollTwoDice
+    {
+        public void DiceGame() 
+        {
+            int desiredSum = 2;
+            Random rnd = new Random();
+            int dice1 = 1;
+            int dice2 = 2;
+            int sum = 0;
+
+            while (true)
+            { 
+                Console.Write("Desired sum : ");
+                desiredSum = Int32.Parse(Console.ReadLine());
+
+                if (desiredSum > 1 && desiredSum < 13) 
+                {
+                    break;
+                }
+
+                Console.WriteLine("Chosen sum can not be achieved with 2 d6 dice!");
+            }
+
+            while (true) 
+            { 
+                dice1 = rnd.Next(1, 7);
+                dice2 = rnd.Next(1, 7);
+                sum = dice1 + dice2;
+
+                Console.WriteLine($"{dice1} and {dice2} = {sum}");
+
+                if (sum == desiredSum) 
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
