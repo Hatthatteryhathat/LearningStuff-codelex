cd0b36bbee9108854fd440446489100cfdb15196
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-11 18:46:14 +0300

    Completed Exercise #11

---
+++ csharp-basics/exercises/Loops/Loops/Exercise11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text1 = "Happy Birthday";
+            string text2 = "MANY THANKS";
+            string text3 = "sPoNtAnEoUs";
+
+            Console.WriteLine(ReverseCase(text1));
+            Console.WriteLine(ReverseCase(text2));
+            Console.WriteLine(ReverseCase(text3));
+            Console.ReadKey();
+        }
+
+        public static string ReverseCase(string text) 
+        {
+            char[] letters = text.ToCharArray();
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (char.IsUpper(letters[i])) 
+                {
+                    letters[i] = char.ToLower(letters[i]);
+                }
+                else 
+                { 
+                    letters[i] = char.ToUpper(letters[i]);
+                }
+            }
+
+            return string.Join("", letters);
+        }
+    }
+}
