a088a2c0229286dd42eef2f7096dc96edfe32b3e
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-11 17:33:40 +0300

    Completed Exercise #8

---
+++ csharp-basics/exercises/Loops/Loops/Exercise8/AsciiFigure.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    internal class AsciiFigure
+    {
+        private const int PyramidSize = 5;
+
+        public  void BuildAsciiFigure() 
+        {
+            int counter = 0;
+
+            for (int i = 1; i < PyramidSize + 1; i++) 
+            { 
+                Console.WriteLine(new String('/', 4 * (PyramidSize - i)) + new String('*', 4 * counter) + new String('\\', 4 * (PyramidSize - i)));
+                counter += 2;
+            }
+        }
+    }
+}
