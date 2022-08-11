170c4ffb162860aa74c808f67e9d6102c60f605b
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-11 18:26:37 +0300

    Completed Exercise #10

---
+++ csharp-basics/exercises/Loops/Loops/Exercise10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberSquare square = new NumberSquare();
+
+            square.DrawNumberSquare();
+
+            Console.ReadKey();
+        }
+    }
+}
