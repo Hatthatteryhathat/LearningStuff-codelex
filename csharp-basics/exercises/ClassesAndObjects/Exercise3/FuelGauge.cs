7e54be9ee510500f5710c3bfa63c9754e43e1d4b
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-15 14:52:35 +0300

    Completed Exercise #3

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise3/FuelGauge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    internal class FuelGauge
+    {
+        private double _currentFuel = 0;
+
+        public double GetFuel() 
+        { 
+            return _currentFuel;
+        }
+
+        public void AddFuel() 
+        {
+            _currentFuel++;
+        }
+
+        public void RemoveFuel()
+        {
+            _currentFuel--;
+        }
+
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise3/Odometer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    internal class Odometer
+    {
+        private double _currentMileage;
+        private double _counter = 0;
+
+        public Odometer(double initialMilage)
+        {
+            _currentMileage = initialMilage;
+        }
+
+        public double GetMileage()
+        {
+            return _currentMileage;
+        }
+        
+        public void AddMileage(FuelGauge fuelGauge)
+        {
+            _currentMileage++;
+            _counter++;
+
+            if (_currentMileage > 999999) 
+            {
+                _currentMileage = 0;
+            }
+
+            if (_counter == 10) 
+            {
+                BurnFuel(fuelGauge);
+                _counter = 0;
+            }
+        }
+
+        private void BurnFuel(FuelGauge fuelGauge)
+        {
+            fuelGauge.RemoveFuel();
+        }
+    }
+}
