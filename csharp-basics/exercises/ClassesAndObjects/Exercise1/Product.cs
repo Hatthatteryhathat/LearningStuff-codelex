138f698c5da96cf717f9ddc13b4f341bef4e2a82
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-15 12:13:56 +0300

    Completed Exercise #1

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    internal class Product
+    {
+        private string _name;
+        private double _price;
+        private int _amount;
+
+        public Product(string name, double priceAtStart, int amountAtStart) 
+        { 
+            _name = name;
+            _price = priceAtStart;
+            _amount = amountAtStart;
+        }
+
+        public void PrintProduct() 
+        {
+            Console.Write($"Name: {_name} || Price: {_price} || Amount: {_amount}");
+        }
+
+        public void ChangePrice() 
+        {
+            Console.Write("\nEnter new price :");
+            _price = double.Parse(Console.ReadLine());
+        }
+
+        public void ChangeAmount() 
+        {
+            Console.Write("\nEnter new amount :");
+            _amount = Int32.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> ProductList = new List<Product>();
+
+            ProductList.Add( new Product("Logitech mouse", 70.00 , 14) );
+            ProductList.Add( new Product("iPhone 5s", 999.99 , 3) );
+            ProductList.Add( new Product("Epson EB-U05", 440.46 , 1) );
+
+            while (true) 
+            {
+                Console.Clear();
+                Console.Write("1|| ");
+                ProductList[0].PrintProduct();
+                Console.Write("\n----------------\n");
+                Console.Write("2|| ");
+                ProductList[1].PrintProduct();
+                Console.Write("\n----------------\n");
+                Console.Write("3|| ");
+                ProductList[2].PrintProduct();
+                Console.Write("\n----------------\n");
+                Console.WriteLine("-=-=-=-=-=-=-=-");
+
+                Console.Write("Choose product to edit : ");
+                int chosenProduct = (int)char.GetNumericValue(Console.ReadKey().KeyChar);
+
+                Console.Write("\n1. Change Price \n2. Change Amount \n-------");
+                Console.Write("\n1 or 2 : ");
+                int chosenAction = (int)char.GetNumericValue(Console.ReadKey().KeyChar);
+                
+                if (chosenAction == 1)
+                {
+                    ProductList[chosenProduct - 1].ChangePrice();
+                }
+                else if (chosenAction == 2) 
+                { 
+                    ProductList[chosenProduct - 1].ChangeAmount();
+                }
+            }
+        }
+    }
+}
