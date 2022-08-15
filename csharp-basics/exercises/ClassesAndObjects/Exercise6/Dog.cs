790a1bbfaf260c993f3d29784c978c96141a37c3
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-15 18:21:36 +0300

    Completed Exercise #6

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise6/Dog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    internal class Dog
+    {
+        private string _name;
+        private string _sex;
+        private Dog _mother;
+        private Dog _father;
+
+        public Dog(string name , string sex) 
+        {
+            this._name = name;
+            this._sex = sex;
+        }
+
+        public string GetFathersName() 
+        {
+            if (_father == null) 
+            {
+                return "Unknown";
+            }
+
+            return _father._name;
+        }
+
+        public void SetMother(Dog mother) 
+        {
+            this._mother = mother;
+        }
+
+        public void SetFather(Dog father)
+        {
+            this._father = father;
+        }
+
+        public bool HasSameMotherAs(Dog otherDog) 
+        {
+            return _mother == otherDog._mother;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise6
+{
+    internal class DogTest
+    {
+        public static DogTest doggos = new DogTest();
+
+        static void Main(string[] args)
+        {
+            Dog dog1 = new Dog("Max", "male");
+            Dog dog2 = new Dog("Rocky", "male");
+            Dog dog3 = new Dog("Sparky", "male");
+            Dog dog4 = new Dog("Buster", "male");
+            Dog dog5 = new Dog("Sam", "male");
+            Dog dog6 = new Dog("Lady", "female");
+            Dog dog7 = new Dog("Molly", "female");
+            Dog dog8 = new Dog("Coco", "female");
+
+            dog1.SetMother(dog6);
+            dog8.SetMother(dog7);
+            dog2.SetMother(dog7);
+            dog4.SetMother(dog6);
+
+            dog1.SetFather(dog2);
+            dog8.SetFather(dog4);
+            dog2.SetFather(dog5);
+            dog4.SetFather(dog3);
+
+            Console.WriteLine("Cocos dad : " + dog8.GetFathersName());
+            Console.WriteLine("Sparkys dad : " + dog3.GetFathersName());
+
+            Console.Write("\nDoes Coco have the same mother as Rocky :: ");
+            Console.Write(dog8.HasSameMotherAs(dog2).ToString());
+            Console.ReadKey();
+        }
+    }
+}
