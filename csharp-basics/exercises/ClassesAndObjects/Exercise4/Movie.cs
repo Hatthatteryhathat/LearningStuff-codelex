b648d43bbdc12b5f6e1252d2b6e3873aa9d61873
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-15 16:28:42 +0300

    Completed Exercise #4

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    internal class Movie
+    {
+        public string title;
+        public string studio;
+        public string rating;
+
+        public Movie(string movieTitle, string movieStudio, string movieRating)
+        {
+            title = movieTitle;
+            studio = movieStudio;
+            rating = movieRating;
+        }
+
+        public Movie(string movieTitle, string movieStudio) 
+        {
+            title = movieTitle;
+            studio = movieStudio;
+            rating = "PG";
+        }
+
+        public void DisplayMovie() 
+        {
+            Console.WriteLine(title);
+            Console.WriteLine(studio);
+            Console.WriteLine(rating);
+            Console.WriteLine("-=-=-=-=-=-=-=-");
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> movieList = new List<Movie>();
+
+            movieList.Add(new Movie("Casino Royale", "Eon Productions", "PG13"));
+            movieList.Add(new Movie("Glass", "Buena Vista International", "PG13"));
+            movieList.Add(new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG"));
+
+            List<Movie> pgMovies = GetPG(movieList);
+
+            foreach (Movie movie in pgMovies) 
+            {
+                movie.DisplayMovie();
+            }
+            
+            Console.ReadLine();
+        }
+
+        public static List<Movie> GetPG(List<Movie> movieList) 
+        {
+            List<Movie> pgMovies = new List<Movie>();
+
+            for (int i = 0; i < movieList.Count; i++) 
+            {
+                if (movieList[i].rating == "PG") 
+                { 
+                pgMovies.Add(movieList[i]);
+                }
+            }
+
+            return pgMovies;
+        } 
+    }
+}
