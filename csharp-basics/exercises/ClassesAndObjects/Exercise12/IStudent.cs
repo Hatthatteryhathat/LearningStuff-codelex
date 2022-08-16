c823e7be4e8e3b1f2b2254506d4780a86e3fa734
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-16 18:24:20 +0300

    Completed Exercise #11

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise12/IStudent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise12
+{
+    internal interface IStudent
+    {
+        public List<string> TestsTaken { get; }
+
+        public void TakeTest(ITestPaper paper, string[] answers);
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise12/ITestPaper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise12
+{
+    internal interface ITestPaper
+    {
+        public string Subject { get; }
+
+        public string[] MarkScheme { get; }
+
+        public string PassMark { get; }
+
+    }
+}
