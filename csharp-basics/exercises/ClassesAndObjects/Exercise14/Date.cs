3139380d00a6f179d137ce8e535845e6a420025c
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-16 21:12:59 +0300

    Completed Exercise #14

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise14/Date.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Exercise14
+{
+    internal class Date
+    {
+        private DateTime _date;
+
+        public Date(int year, int month, int day) 
+        { 
+            _date = new DateTime(year, month, day);
+        }
+
+        public string WeekdayInDutch() 
+        {
+            CultureInfo dutchCulture = new System.Globalization.CultureInfo("nl-NL");
+            string dayName = dutchCulture.DateTimeFormat.GetDayName(_date.DayOfWeek);
+
+            return dayName;
+        }
+    }
+}
