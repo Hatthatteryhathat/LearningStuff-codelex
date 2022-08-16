4f68b0ce0ed69bdf0679347571a14320aeef6d78
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-16 16:27:15 +0300

    Completed Exercise #10 - VideoStore

--- csharp-basics/exercises/ClassesAndObjects/VideoStore/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/Program.cs
@@ -8,20 +8,30 @@ namespace VideoStore
 {
     class Program
     {
-        private const int _countOfMovies = 3;
+        private const int _CountOfMovies = 3;
         private static VideoStore _videoStore = new VideoStore();
+
         private static void Main(string[] args)
         {
+            bool test = true;
+            if (test) 
+            {
+                TestVideoStore();
+                Environment.Exit(0);
+            }
             while (true)
             {
+                Console.Clear();
                 Console.WriteLine("Choose the operation you want to perform ");
                 Console.WriteLine("Choose 0 for EXIT");
                 Console.WriteLine("Choose 1 to fill video store");
                 Console.WriteLine("Choose 2 to rent video (as user)");
                 Console.WriteLine("Choose 3 to return video (as user)");
                 Console.WriteLine("Choose 4 to list inventory");
-
+                Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                Console.Write("Input choice :: ");
                 int n = Convert.ToByte(Console.ReadLine());
+                Console.WriteLine("-------");
 
                 switch (n)
                 {
@@ -52,12 +62,12 @@ namespace VideoStore
 
         private static void FillVideoStore()
         {
-            for (var i = 0; i < _countOfMovies; i++)
+            for (var i = 0; i < _CountOfMovies; i++)
             {
-                Console.WriteLine("Enter movie name");
+                Console.Write("Enter movie name :: ");
                 string movieName = Console.ReadLine();
 
-                Console.WriteLine("Enter rating");
+                Console.Write("Enter rating :: ");
                 int rating = Convert.ToInt16(Console.ReadLine());
 
                 _videoStore.AddVideo(movieName);
@@ -67,16 +77,53 @@ namespace VideoStore
 
         private static void RentVideo()
         {
-            Console.WriteLine("Enter movie name");
+            Console.Write("Enter movie name :: ");
             string movieName = Console.ReadLine();
             _videoStore.Checkout(movieName);
         }
 
         private static void ReturnVideo()
         {
-            Console.WriteLine("Enter movie name");
+            Console.Write("Enter movie name :: ");
             string movieName = Console.ReadLine();
             _videoStore.ReturnVideo(movieName);
+        }
+
+        public static void TestVideoStore() 
+        {
+            // test adding videos
+            Console.WriteLine("Adding 3 videos:");
+            _videoStore.AddVideo("The Matrix");
+            _videoStore.AddVideo("Godfather II");
+            _videoStore.AddVideo("Star Wars Episode IV: A New Hope");
+
+            // test adding ratings
+            _videoStore.TakeUsersRating(7, "The Matrix");
+            _videoStore.TakeUsersRating(6, "The Matrix");
+
+            _videoStore.TakeUsersRating(10, "Godfather II");
+            _videoStore.TakeUsersRating(9 , "Godfather II");
+            _videoStore.TakeUsersRating(2 , "Godfather II");
+
+            _videoStore.TakeUsersRating(8, "Star Wars Episode IV: A New Hope");
+            _videoStore.TakeUsersRating(4, "Star Wars Episode IV: A New Hope");
+
+            // test renting
+            Console.WriteLine("Renting 3 videos:");
+            _videoStore.Checkout("The Matrix");
+            _videoStore.Checkout("Godfather II");
+            _videoStore.Checkout("Star Wars Episode IV: A New Hope");
+
+            // test returning
+            Console.WriteLine("Returning 2 videos:");
+            _videoStore.ReturnVideo("The Matrix");
+            _videoStore.ReturnVideo("Star Wars Episode IV: A New Hope");
+
+            // test displaying video list
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+            _videoStore.ListInventory();
+
+            Console.ReadKey();
         }
     }
 }
--- csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
@@ -5,34 +5,75 @@ namespace VideoStore
 {
     class VideoStore
     {
-
-        public VideoStore()
-        {
-            
-        }
+        List<Video> videoList = new List<Video>();
 
         public void AddVideo(string title)
         {
-            
+            videoList.Add(new Video(title));
         }
         
         public void Checkout(string title)
         {
-
+            if (videoList.Find(video => video?.Title == title) == null)
+            {
+                Console.WriteLine($" >> Error: Video by name '{title}' not found!");
+            }
+            else if (!videoList.Find(video => video?.Title == title).Available())
+            {
+                Console.WriteLine($" >> Sorry. Video by name '{title}' is allready checked out!");
+            }
+            else 
+            {
+                videoList.Find(video => video.Title == title).BeingCheckedOut();
+            }
+            
+            Console.WriteLine("-Press any-key to continnue-");
+            Console.ReadKey();
         }
 
         public void ReturnVideo(string title)
         {
+            if (videoList.Find(video => video?.Title == title) == null)
+            {
+                Console.WriteLine($" >> Error: Video by name '{title}' not found!");
+            }
+            else if (videoList.Find(video => video?.Title == title).Available())
+            {
+                Console.WriteLine($" >> Sorry. Video by name '{title}' was not checked out!");
+            }
+            else 
+            {
+                videoList.Find(video => video.Title == title).BeingReturned();
+                Console.WriteLine("-- Video Returned!");
+
+                Console.Write("Enter video rating(0/10) :: ");
+                double rating = double.Parse(Console.ReadLine());
+                videoList.Find(video => video.Title == title).ReceivingRating(rating);
+            }
+
+            Console.WriteLine("-Press any-key to continnue-");
+            Console.ReadKey();
         }
 
         public void TakeUsersRating(double rating, string title)
         {
-            
+            videoList.Find(video => video.Title == title).ReceivingRating(rating);
         }
 
         public void ListInventory()
         {
-            
+            for (int i = 1; i <= videoList.Count; i++) 
+            {
+                Console.WriteLine($"||{i}. {videoList[i-1].ToString()}");
+            }
+
+            if (videoList.Count == 0) 
+            {
+                Console.WriteLine(" >> Video store empty!");
+            }
+
+            Console.WriteLine("-Press any-key to continnue-");
+            Console.ReadKey();
         }
     }
 }
