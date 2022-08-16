196bcf37e9c3e75ab6afb74c4ce1ef042b6b8756
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-16 20:42:34 +0300

    Complete Exercise #13

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise13/Ingredient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise13
+{
+    internal class Ingredient
+    {
+        private string _name;
+        private double _cost;
+
+        public Ingredient(string name, double cost) 
+        { 
+            _name = name;
+            _cost = cost;
+        }
+
+        public string Name => _name;
+
+        public double Cost => _cost;
+    }
+}
