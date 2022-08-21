0f4cac42981cd1599ecb26e3dbf683def2c2c70b
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-22 01:55:49 +0300

    Completed Exercise #4

---
+++ csharp-basics/exercises/Collections/Exercise4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> uniqueNames = new HashSet<string>();
+
+            while (true) 
+            { 
+                Console.Write("Enter name: ");
+                string input = Console.ReadLine();
+
+                if (input != "")
+                {
+                    uniqueNames.Add(input);
+                }
+                else 
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("-=-=-=-=-=-=-=-");
+            Console.WriteLine($"Unique name list contains: {string.Join(" ", uniqueNames)}");
+            Console.ReadKey();
+        }
+    }
+}
