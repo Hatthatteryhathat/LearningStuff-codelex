2a69b218242e5220e2ef3f0f85a6dae46e58af58
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-22 03:59:51 +0300

    Compleated Exercise #6 - WordCount

--- csharp-basics/exercises/Collections/WordCount/Program.cs
+++ csharp-basics/exercises/Collections/WordCount/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,17 @@ namespace WordCount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var text = File.ReadAllText("lear.txt");
+            
+            string[] lines = text.Split('\n');
+            string[] words = text.Replace("\n", " ").Split(' ','\'').Where(word => word != "").ToArray();
+            char[] chars = text.ToCharArray().Where(ch => ch != '\n' && ch != '\r').ToArray();
+
+            Console.WriteLine($"Lines : {lines.Length}");
+            Console.WriteLine($"Words : {words.Length}");
+            Console.WriteLine($"Chars : {chars.Length}");
+
+            Console.ReadKey();
         }
     }
 }
