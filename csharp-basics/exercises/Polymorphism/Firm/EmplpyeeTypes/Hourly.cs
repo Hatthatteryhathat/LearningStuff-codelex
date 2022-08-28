e7431a8a95b442c89203ed903f297ee6d9c488f7
Hatthatteryhathat <86327391+Hatthatteryhathat@users.noreply.github.com>
2022-08-28 19:10:53 +0300

    Completed Exercise #4 - Firm

--- csharp-basics/exercises/Polymorphism/Firm/EmplpyeeTypes/Hourly.cs
+++ csharp-basics/exercises/Polymorphism/Firm/EmplpyeeTypes/Hourly.cs
@@ -1,15 +1,16 @@
-namespace Firm
+namespace Firm.EmplpyeeTypes
 {
     public class Hourly : Employee
     {
-        private int _hoursWorked;
+        protected int hoursWorked;
+
         //-----------------------------------------------------------------
         // Sets up this hourly employee using the specified information.
         //-----------------------------------------------------------------
         public Hourly(string eName, string eAddress, string ePhone, string socSecNumber, double rate) : base(eName,
             eAddress, ePhone, socSecNumber, rate)
         {
-            _hoursWorked = 0;
+            hoursWorked = 0;
         }
 
         //-----------------------------------------------------------------
@@ -18,26 +19,26 @@ namespace Firm
         //-----------------------------------------------------------------
         public void AddHours(int moreHours)
         {
-            _hoursWorked += moreHours;
+            hoursWorked += moreHours;
         }
 
         //-----------------------------------------------------------------
         // Computes and returns the pay for this hourly employee.
         //-----------------------------------------------------------------
-        public override double Pay() 
+        public override double Pay() { 
 
-            var payment = payRate * _hoursWorked;
-            _hoursWorked = 0;
+            var payment = payRate * hoursWorked;
+        hoursWorked = 0;
             return payment;
         }
 
         //-----------------------------------------------------------------
         // Returns information about this hourly employee as a string.
         //-----------------------------------------------------------------
-        public override string ToString() 
+        public override string ToString()
         {
             var result = base.ToString();
-            result += "\nCurrent hours: " + _hoursWorked;
+            result += "\nCurrent hours: " + hoursWorked;
             return result;
         }
     }
